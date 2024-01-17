ff75fe5ab0fc8e9059a32bd01e80f13da221091e
mauren-nieto <maurenbriggitte@gmail.com>
2024-01-16 20:57:02 -0500

    se agrego funcionadidad y estilos al boton saber mas

---
+++ JS/main.js
@@ -0,0 +1,89 @@
+const grid = new Muuri('.grid',{
+    layout:{
+        rounding:false
+    }
+})
+/*---------------Cargue de imagenes--------------*/
+window.addEventListener('load', () => {
+    grid.refreshItems().layout();
+    grid.filter(`[data-categoria="mundo ilustrado"]`);
+    document.getElementById('grid').classList.add('imagenes-cargadas');
+
+/*-------------Visualizacion de la ventana del perfil al dar click-------------*/
+const perfil = document.getElementById('perfil');
+const botones = document.querySelectorAll('.inicio .intereses-y-redes button');
+//const lampara = document.querySelector('.ambiente .lampara');
+
+botones.forEach(function (boton) {
+    boton.addEventListener('click', function () {
+        perfil.classList.add('activo');
+       // lampara.classList.add('activo');  // Agrega la clase 'activo' a la lámpara
+    });
+});
+
+document.querySelector('#boton-cerrar-popup').addEventListener('click', () => {
+    perfil.classList.remove('activo');
+    //lampara.classList.remove('activo');  // Elimina la clase 'activo' de la lámpara
+});
+});
+/*const perfil = document.getElementById('perfil');
+const ambiente = document.getElementById('ambiente');
+const botones = document.querySelectorAll('.inicio .intereses-y-redes button');
+const lampara = document.querySelectorAll('.ambiente .lampara');
+
+
+botones.forEach(function (boton) {
+    boton.addEventListener('click', function () {
+        perfil.classList.add('activo');
+    });
+});
+  document.querySelector('#boton-cerrar-popup').addEventListener('click',()=>{
+    perfil.classList.remove('activo');
+  });
+});*/
+
+
+/*---------------Indicador de color ----------------*/
+    const enlaces = document.querySelectorAll('#categorias a');
+    enlaces.forEach((elemento) => { elemento.addEventListener('click', (evento) => {
+        evento.preventDefault();
+        enlaces.forEach((enlaces)=> enlaces.classList.remove('activo'));
+        evento.target.classList.add('activo');
+
+/*-----  --- visualizacion de las imagenes segun la categoria---------*/
+    const categoria = evento.target.innerHTML.toLowerCase();
+       categoria === 'todos' ? grid.filter('[data-categoria]') : 
+       grid.filter(`[data-categoria="${categoria}"]`);
+       });
+   
+   /*-------------Visualizacion de imagenes al dar click-------------*/
+   const overlay = document.getElementById('overlay');
+   document.querySelectorAll('.grid .item img') .forEach((elemento)=>{
+    elemento.addEventListener('click', ()=>{
+        const ruta= elemento.getAttribute('src');
+        const descripcion = elemento.parentNode.parentNode.dataset.descripcion;
+
+        overlay.classList.add('activo');
+        document.querySelector('#overlay img').src = ruta;
+        document.querySelector('#overlay .descripcion').innerHTML = descripcion;
+        });
+   });
+    /*--------------------------- visualizacion de videos al dar click------------*/
+   const overlay2 = document.getElementById('overlay');
+   document.querySelectorAll('.grid .item video source') .forEach((elemento)=>{
+      const ruta = elemento.getAttribute('src');
+      const descripcion = elemento.parentNode.parentNode.dataset.descripcion;
+
+   elemento.addEventListener('click', ()=>{  
+         overlay2.classList.add('activo');
+         document.querySelector('#overlay video').src = ruta;
+         document.querySelector('#overlay .descripcion').innerHTML = descripcion;
+         })
+    });
+
+   /*---------------- boton de cerrar la imagen------------- */
+
+   document.querySelector('#btn-cerrar-popup').addEventListener('click', () =>{
+    overlay.classList.remove('activo');
+   })
+});
